b2f4e45ca37757c1c617c7c81f3a18e473ca4a59
CallmeMrhu <mrhu666@163.com>
2017-11-10 21:36:48 +0800

    2017.11.10 last version

--- MRLR_Optimization_own.py
+++ MRLR_Optimization_own.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import random
+from scipy.stats import logistic
 
 # dataset structure：
 # matrix R：user * item
@@ -15,6 +16,7 @@ class MRLR():
         self.C = C
         self.lamda = lamda
         self.alpha1 = alpha1
+        self.alpha2 = 1-alpha1
         self.alpha3 = alpha3
         self.gamma = gamma
         self.d = d
@@ -52,40 +54,54 @@ class MRLR():
                 is_zero_size = is_zero.size
                 if non_zero_size < 2:
                     continue
-                for i in range(non_zero_size - 1):
-                    # u_vi = list([i, non_zero[0][j]])
-                    for k in range(i + 1, non_zero_size):
-                        # negtive sampling:N
-                        negtive_sample = random.sample(range(is_zero_size), N)
-                        for n in range(N):
-                            g = negtive_sample[n]
-                        # calculate negtive sampling
+                for id_one in range(non_zero_size - 1):
+                    i = non_zero[0][id_one]
+                    for id_two in range(i + 1, non_zero_size):
+                        k = non_zero[0][id_two]
                         # calculate gradient
-                        # P(vk|up,vi,sita)
-                        # P(vi|up,vk,sita)
                         # update up
+                        # P(vk|up,vi,sita)
+                        kpi_up = v[k] * (1-logistic.cdf(self.alpha1 * np.dot(u[p],v[k])+self.alpha2 * np.dot(v[i],v[k])))
+                        # P(vi|up,vk,sita)
+                        ipk_up = v[i] * (1-logistic.cdf(self.alpha1 * np.dot(u[p],v[i])+self.alpha2 * np.dot(v[k],v[i])))
+                        # negtive sampling:N
+                        gpi_up = 0
+                        gpk_up = 0
+                        negtive_sample = random.sample(range(is_zero_size), N)
+                        for index_three in range(N):
+                            id_three = negtive_sample[index_three]
+                            g = is_zero[0][id_three]
+                            gpi_up += v[g] * (1 - logistic.cdf(-1 * (self.alpha1 * np.dot(u[p], v[g]) + self.alpha2 * np.dot(v[i], v[g]))))
+                            gpk_up += v[g] * (1 - logistic.cdf(-1 * self.alpha1 * np.dot(u[p], v[k]) + self.alpha2 * np.dot(v[k], v[g])))
+                        # calculate negtive sampling
+                        u[p] = u[p] + self.gamma * (kpi_up + ipk_up - gpi_up - gpk_up)
                         # update vi
-                        # update cl
-
-
-
+                        kpi_vi = kpi_up
+                        ipk_vi = u[p] * (1-logistic.cdf(self.alpha1 * np.dot(u[p],v[k])+self.alpha2 * np.dot(v[i],v[k])))
+                        gpi_vi = gpi_up
+                        gpk_vi = 0
+                        v[i] = v[i] + self.gamma * (kpi_vi + ipk_vi + gpi_vi + gpk_vi)
+                        # update cl : category
+                        # 待写
                     # number of Dr
                     number_Dr = non_zero_size
                     sample_result = random.sample(range(is_zero_size), number_Dr)
                     for sample_index in range(number_Dr):
-                        j = sample_result[sample_index]
-
+                        id_four = sample_result[sample_index]
+                        j = is_zero[0][id_four]
+                        jpi_up = (v[i]-v[j]) * (1 - logistic.cdf(np.dot(u[p],v[i])-np.dot(u[p],v[j])))
+                        gpj_up = 0
                         negtive_sample = random.sample(range(is_zero_size), N)
-                        for n in range(N):
-                            g = negtive_sample[n]
+                        for index_three in range(N):
+                            id_five = negtive_sample[index_three]
+                            g = is_zero[0][id_five]
+                            gpj_up += (v[g]-v[j])*(1 - logistic.cdf(-1 * (np.dot(u[p],v[j])-np.dot(u[p],v[g]))))
+                        u[p] = u[p] + self.gamma * (jpi_up + gpj_up)
+                        jpi_vi = u[p] * (1 - logistic.cdf(np.dot(u[p],v[i])-np.dot(u[p],v[j])))
+                        v[i] = v[i] + self.gamma * (jpi_vi)
 
-                            # calculate negtive sampling
-                            # calculate gradient
-                            # P(vj,vi|up,sita)
-                            # update up
-                            # update vi
-                            # update cl
-
+            # J has converged, and then break;
+            # 待写
 
 
 
